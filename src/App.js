57814c9d977c69a3c23b692158143026844055b1
Florin Ionescu <florineugenionescu@gmail.com>
2023-06-04 13:43:43 +0300

    Create and initialize 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Page404 from "./pages/Page404/Page404";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <Page404 />
     </div>
   );
 }
---
+++ src/pages/Page404/Page404.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Page404.module.css";
+import Container from "react-bootstrap/Container";
+
+function Page404() {
+  return (
+    <div
+      className={`${styles.page404} bg-primary text-white d-flex flex-column justify-content-center align-items-center`}
+    >
+      <Container className="d-flex flex-column justify-content-center align-items-center">
+        <p className="h4 text-center">
+          Avem o știre nu tocmai grozavă... pagina pe care ai încercat să o
+          accesezi nu (mai) există.
+        </p>
+        <strong className={`${styles.error404}`}>404 🙁</strong>
+        <p className="h4 text-center">
+          Hai înapoi pe site să vezi o nouă știre!
+        </p>
+      </Container>
+    </div>
+  );
+}
+
+export default Page404;
