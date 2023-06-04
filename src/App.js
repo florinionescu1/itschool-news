a4d4da777515a001f2b4f7c423af056559cdb2ca
Florin Ionescu <florineugenionescu@gmail.com>
2023-06-04 15:59:47 +0300

    Initialize context and reducer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Page404 from "./pages/Page404/Page404";
@@ -5,10 +6,22 @@ import Home from "./pages/Home/Home";
 import Favorites from "./pages/Favorites/Favorites";
 import NewsCategory from "./pages/NewsCategory/NewsCategory";
 import NewsDetails from "./pages/NewsDetails/NewsDetails";
+import { FavoritesContext } from "./store/Favorites/context";
+import { initialState, favoritesReducer } from "./store/Favorites/reducer";
 
 function App() {
+  const [favoritesState, favoritesDispatch] = useReducer(
+    favoritesReducer,
+    initialState
+  );
+  const favoritesContextValue = {
+    favoritesState,
+    favoritesDispatch,
+  };
+
   return (
     <div className="App">
+     <FavoritesContext.Provider value={favoritesContextValue}>
        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
@@ -16,6 +29,7 @@ function App() {
         <Route path="/news/:newsId/*" element={<NewsDetails />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
+     </FavoritesContext.Provider>
     </div>
   );
 }
