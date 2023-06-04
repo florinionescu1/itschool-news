1fadd2b3e4c7ad578edd06fc4dd95eb9dddff1c1
Florin Ionescu <florineugenionescu@gmail.com>
2023-06-04 13:30:45 +0300

    Create and initialize footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+function Footer() {
+  return (
+    <footer className="bg-dark">
+      <Container>
+        <p className="text-light text-center m-0 py-3">
+          IT School România ©{new Date().getFullYear()}. Toate drepturile
+          rezervate.
+        </p>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
