4d8b92b4625d9f9b5df0b75f366af38889977d1f
Florin Ionescu <florineugenionescu@gmail.com>
2023-06-04 13:25:15 +0300

    Create and initialize header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./components/Header/Header";
+
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
