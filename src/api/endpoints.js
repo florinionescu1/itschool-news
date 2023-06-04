4bc45f0fdb08a448eebb1122161d32fbe14cca96
Florin Ionescu <florineugenionescu@gmail.com>
2023-06-04 14:21:51 +0300

    Add api endpoints

---
+++ src/api/endpoints.js
@@ -0,0 +1,17 @@
+const API_KEY = "33591ae5-fd2f-41d6-a1e1-b98c2797a96b";
+
+export function getNewsCategoriesEndpoint(
+  category,
+  pageNumber = 1,
+  pageSize = 20
+) {
+  const queryParams = `?api-key=${API_KEY}&section=${category}&show-fields=all&page-size=${pageSize}&page=${pageNumber}`;
+
+  return `https://content.guardianapis.com/search${queryParams}`;
+}
+
+export function getNewsDetailsEndpoint(newsId) {
+  const queryParams = `?api-key=${API_KEY}&show-fields=all`;
+
+  return `https://content.guardianapis.com/${newsId}${queryParams}`;
+}
